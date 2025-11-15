c166c4af507b56d010050c84b02bbc61129a8aa4
Adrien Verstrepen <adrienverstrepen@gmail.com>
2025-11-15 15:28:43 +0100

    Correction d'un espace dans l'affichage de la valeur des taux d'infection

--- src/view/widgets/sliders/taux_infectes.py
+++ src/view/widgets/sliders/taux_infectes.py
@@ -15,7 +15,7 @@ class Slider_infectes(QSlider):
 		self.son_menu = menu
 
 	def changement_valeur(self):
-		self.son_menu.label_infectes.setText(f"{self.texte}: {self.son_menu.slider_infectes.value()}%")
+		self.son_menu.label_infectes.setText(f"{self.texte} : {self.son_menu.slider_infectes.value()}%")
 
 	def slider_relache(self):
 		print(self.value())
